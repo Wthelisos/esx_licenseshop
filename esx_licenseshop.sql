36009d1acc89c6b702451f4e0f76ec32f311e4a5
wthelisos <wthelisos@gmail.com>
2021-12-09 14:46:49 +0100

    subida

---
+++ esx_licenseshop.sql
@@ -0,0 +1,20 @@
+USE `es_extended`;
+
+INSERT INTO `licenses` (`type`, `label`) VALUES
+	('weed_processing', 'Weed Processing License'),
+	('license_aircraft', 'licencia piloto'),
+	('license_boating', 'licencia de barco'),
+	('license_melee', 'licencia de armas cuerpo a cuerpo'),
+	('license_handgun', 'Licencia de Pistola'),
+  	('license_smg', 'Licencia de subfusil'),
+  	('license_shotgun', 'Licencia de escopeta'),
+  	('license_assault', 'Licencia de rifle de asalto'),
+  	('license_lmg', 'Licencia de ametralladora ligera'),
+  	('license_sniper', 'Licencia de rifle francotirador'),
+  	('license_commercial', 'Licencia Comercial'),
+  	('license_drivers', 'Licencia de Conducir'),
+  	('license_driversp', 'Permiso de conducir'),
+  	('license_motorcycle', 'Licencia de Motocicleta'),
+  	('license_weed', 'Licencia de marihuana'),
+  	('license_weapon', 'Licencia de Armas')
+;
---
+++ locales/en.lua
@@ -0,0 +1,30 @@
+Locales['en'] = {
+  ['dmv_school'] = 'Prueba Abierta del DMV',
+  ['buy_license'] = 'Quiere Comprar una Licencia?',
+  ['where_to_go'] = 'Elige adonde ir',
+  ['press_access'] = 'Presiona ~INPUT_CONTEXT~ para acceder al menu de licencias',
+  ['not_enough_money'] = 'no tienes suficiente dinero',
+  ['blip_license_shop'] = 'Tienda de licencias',
+  ['need_dmv'] = 'Usted debe tener un permiso de conducir',
+  ['shop_menu_item'] = '$%s',
+  -- esx_advancedvehicleshop Licenses
+  ['license_aircraft'] = 'Licencia de piloto',
+  ['license_boating'] = 'Licencia de barco',
+  -- esx_advancedweaponshop Licenses
+  ['license_melee'] = 'Licencia de arma cuerpo a cuerpo',
+  ['license_handgun'] = 'Licencia de Pistola',
+  ['license_smg'] = 'Licencia de subfusil',
+  ['license_shotgun'] = 'Licencia de escopeta',
+  ['license_assault'] = 'Licencia de rifle de asalto',
+  ['license_lmg'] = 'Licencia de ametralladora ligera',
+  ['license_sniper'] = 'Licencia de rifle francotirador',
+  -- esx_dmvschool Licenses
+  ['license_commercial'] = 'Licencia Comercial',
+  ['license_drivers'] = 'Licencia de Conducir',
+  ['license_driversp'] = 'Permiso de conducir',
+  ['license_motorcycle'] = 'Licencia de Motocicleta',
+  -- esx_drugs Licenses
+  ['license_weed'] = 'Licencia de marihuana',
+  -- esx_weashop Licenses
+  ['license_weapon'] = 'Licencia de Armas',
+}
